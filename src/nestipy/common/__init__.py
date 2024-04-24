496b23deb0135f90afdd4db84dca22e7552125a5
tsiresymila <tsiresymila@gmail.com>
2024-04-24 11:52:12 +0300

    Request Upload file

--- src/nestipy/common/__init__.py
+++ src/nestipy/common/__init__.py
@@ -3,7 +3,7 @@ from .config_module import ConfigModule, ConfigService
 from .decorator import Module, Controller, Injectable, Route, Get, Post, Patch, Put, Delete
 from .exception import HttpException, HttpStatusMessages, HttpStatus, UseFilters, Catch, ExceptionKey, ExceptionFilter
 from .guards import CanActivate, UseGuards
-from .http_ import Request, Response, Websocket
+from .http_ import Request, Response, Websocket, UploadFile
 from .interceptor import NestipyInterceptor, UseInterceptors
 from .middleware import cors, NestipyMiddleware
 from .template import Render, TemplateEngine
@@ -32,6 +32,7 @@ __all__ = [
     "Response",
     "Request",
     "Websocket",
+    "UploadFile",
     "NestipyInterceptor",
     "UseInterceptors",
     "cors",
