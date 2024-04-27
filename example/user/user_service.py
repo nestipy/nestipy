1965f54f289ab90607ce920b58a10226a04be360
tsiresymila <tsiresymila@gmail.com>
2024-04-27 18:31:59 +0300

    [feat]: add discover service

--- example/user/user_service.py
+++ example/user/user_service.py
@@ -1,8 +1,15 @@
 from nestipy_ioc import Inject
 
 from nestipy.common import Injectable
+from nestipy.core import OnInit
 
 
 @Injectable()
-class UserService:
+class UserService(OnInit):
     tes: Inject['TEST']
+
+    async def on_startup(self):
+        print('On init service')
+
+    async def on_shutdown(self):
+        pass
