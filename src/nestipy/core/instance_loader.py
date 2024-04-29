584ac32b235bbf553892d841041fa2c7a0c269f0
tsiresymila <tsiresymila@gmail.com>
2024-04-29 14:50:00 +0300

    [release] v0.2.7 update readme

--- src/nestipy/core/instance_loader.py
+++ src/nestipy/core/instance_loader.py
@@ -25,8 +25,6 @@ class InstanceLoader:
     """
     _is_controller: bool = False
     _module_instances: list = []
-    _controller_instances: list = []
-    _provider_instances: list = []
     graphql_instance: GraphqlModule = None
     discover: DiscoverService = DiscoverService()
 
@@ -39,11 +37,9 @@ class InstanceLoader:
                 continue
             providers = await self._create_providers(module)
             self.discover.add_provider(*providers)
-            self._provider_instances.append(*providers)
             self._is_controller = True
             controllers = await self._create_controllers(module)
             self.discover.add_controller(*controllers)
-            self._controller_instances.append(*controllers)
             self._is_controller = False
             instance = await self.create_instance(module)
             self._module_instances.append(module)
