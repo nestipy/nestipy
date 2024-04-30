05d08ad1cb2748fd9c6608a5f9a63223744bb2b5
tsiresymila <tsiresymila@gmail.com>
2024-04-30 14:41:12 +0300

    [feat] combine in one dependency

--- example/user/user_service.py
+++ example/user/user_service.py
@@ -1,11 +1,11 @@
 from nestipy.ioc import Inject
 
 from nestipy.common import Injectable
-from nestipy.core import OnInit
+from nestipy.core import OnInit, OnDestroy
 
 
 @Injectable()
-class UserService(OnInit):
+class UserService(OnInit, OnDestroy):
     tes: Inject['TEST']
 
     async def on_startup(self):
