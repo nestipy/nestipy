68e61c7e30d4be6569eaa13d46bd47095088426c
tsiresymila <tsiresymila@gmail.com>
2024-06-14 21:38:25 +0300

    [tag]: 0.4.6

--- src/nestipy/common/logger.py
+++ src/nestipy/common/logger.py
@@ -1,3 +1,7 @@
 import logging
 
+from rich.console import Console
+from rich.style import Style
+
 logger = logging.getLogger('uvicorn')
+console = Console(color_system="truecolor", style=Style(bold=True, color='green'))
