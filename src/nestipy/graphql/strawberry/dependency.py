0abb4e5b8c2dd7b162fa034cc75e6be52df21760
Tsiresy Milà <tsiresymila@gmail.com>
2024-07-26 17:43:10 +0300

    [GQL]: add root and info strawberry

---
+++ src/nestipy/graphql/strawberry/dependency.py
@@ -0,0 +1,18 @@
+from typing import Optional, Type
+
+from nestipy.ioc.dependency import create_type_annotated
+from nestipy.ioc.context_container import RequestContextContainer
+from nestipy.metadata import CtxDepKey
+
+
+def args_callback(_name: str, _token: Optional[str], _type_ref: Type, _request_context: RequestContextContainer):
+    args = _request_context.execution_context.switch_to_graphql().get_args()
+    return args.get(_token or _name)
+
+
+def Root():
+    return create_type_annotated(args_callback, CtxDepKey.Args)("root")
+
+
+def Info():
+    return create_type_annotated(args_callback, CtxDepKey.Args)("info")
