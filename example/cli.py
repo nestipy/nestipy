0d121f9cd34b691458eec34db3444d8dd1576f36
Tsiresy Milà <tsiresymila@gmail.com>
2024-12-16 18:08:26 +0300

    init commander

---
+++ example/cli.py
@@ -0,0 +1,5 @@
+from nestipy.commander import CommandFactory
+
+from app_module import AppModule
+
+command = CommandFactory.create(AppModule)
---
+++ src/nestipy/commander/__init__.py
@@ -0,0 +1,6 @@
+from .decorator import Command
+from .commander import NestipyCommander
+from .abstract import BaseCommand
+from .factory import CommandFactory
+
+__all__ = ["Command", "NestipyCommander", "BaseCommand", "CommandFactory"]
