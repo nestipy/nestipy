57545e1446e1831eca811f94239aa642016132cb
Tsiresy Milà <tsiresymila@gmail.com>
2025-07-28 21:19:55 +0300

    update(proto): update version

--- src/nestipy/microservice/client/grpc/microservice_pb2.pyi
+++ src/nestipy/microservice/client/grpc/microservice_pb2.pyi
@@ -1,18 +1,37 @@
-class DataRequest:
-    def __init__(self, topic: str, data: str):
-        pass
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
 
-class SubRequest:
-    def __init__(self, topic: str):
-        pass
+DESCRIPTOR: _descriptor.FileDescriptor
 
-class UnsubRequest:
-    def __init__(self, topic: str):
-        pass
+class SubRequest(_message.Message):
+    __slots__ = ("topic",)
+    TOPIC_FIELD_NUMBER: _ClassVar[int]
+    topic: str
+    def __init__(self, topic: _Optional[str] = ...) -> None: ...
 
-class DataResponse:
-    def __init__(self, topic: str, data: str):
-        pass
+class UnsubRequest(_message.Message):
+    __slots__ = ("topic",)
+    TOPIC_FIELD_NUMBER: _ClassVar[int]
+    topic: str
+    def __init__(self, topic: _Optional[str] = ...) -> None: ...
 
-class Empty:
-    pass
+class DataRequest(_message.Message):
+    __slots__ = ("topic", "data")
+    TOPIC_FIELD_NUMBER: _ClassVar[int]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    topic: str
+    data: str
+    def __init__(self, topic: _Optional[str] = ..., data: _Optional[str] = ...) -> None: ...
+
+class DataResponse(_message.Message):
+    __slots__ = ("data", "topic")
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    TOPIC_FIELD_NUMBER: _ClassVar[int]
+    data: str
+    topic: str
+    def __init__(self, data: _Optional[str] = ..., topic: _Optional[str] = ...) -> None: ...
+
+class Empty(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
